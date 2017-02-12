87dbe438834a10e7a5b40d15fe8bac83a7d50ecc
Beau Carnes <beaucarnes@gmail.com>
2017-02-12 18:30:25 +0000

    create basic layout and get search working

---
+++ readme.md
@@ -0,0 +1,10 @@
+Consumer Key	ArnyMwZa2JMxtel0wuODtg
+Consumer Secret	nzKWdyHnFRtNoB5a9OLdXLAO0fM
+Token	jK7OQnZpvbT_CXlXVq7U5rIo21pjf7eA
+Token Secret	YOMrcPaqVdbj-oV627591iqjLqU
+
+https://api.yelp.com/v2/search/?location=saginaw, mi&category_filter=coffee
+
+https://www.yelp.com/developers/api_console
+
+https://github.com/olalonde/node-yelp
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+// Request API access: http://www.yelp.com/developers/getting_started/api_access
+var Yelp = require('yelp');
+
+var yelp = new Yelp({
+  consumer_key: 'ArnyMwZa2JMxtel0wuODtg',
+  consumer_secret: 'nzKWdyHnFRtNoB5a9OLdXLAO0fM',
+  token: 'jK7OQnZpvbT_CXlXVq7U5rIo21pjf7eA',
+  token_secret: 'YOMrcPaqVdbj-oV627591iqjLqU',
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var searchResults = [];
+    if (req.query.location) {
+      yelp.search({ term: 'coffee', location: req.query.location })
+          .then(function (data) {
+            for(var i in data.businesses) {
+                searchResults.push({"id": data.businesses[i].id,
+                                    "name": data.businesses[i].name,
+                                    "snippet": data.businesses[i].snippet_text,
+                                    "image_url": data.businesses[i].image_url
+                })
+            };
+            
+            res.render('index', { searchResults: searchResults, location: req.query.location });
+          })
+          .catch(function (err) {
+            console.error(err);
+            res.render('index', { searchResults: searchResults });
+          });
+    } else {
+      res.render('index', { searchResults: null, location: "Where are you?" });
+    };
+
+  
 });
 
 module.exports = router;
