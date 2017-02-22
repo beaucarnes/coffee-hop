15feba7f6063cb9fe45ac16ddbe4cb7d33aae818
Beau Carnes <beaucarnes@gmail.com>
2017-02-22 11:07:04 +0000

    fixing buttons

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -1,18 +1,18 @@
-function updateCount(placeID, visiting) {
+function updateCount(placeID) {
   console.log("updateCount called")
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     console.log("request sent")
     console.log(this.responseText)
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById(placeID).innerHTML = this.responseText;
-      if (visiting) {
+      if (document.getElementById(placeID).innerHTML > this.responseText)
         document.getElementById(placeID).classList.remove("btn-success");
         document.getElementById(placeID).classList.add("btn-info");
       } else {
         document.getElementById(placeID).classList.remove("btn-info");
         document.getElementById(placeID).classList.add("btn-success");
       }
+      document.getElementById(placeID).innerHTML = this.responseText;
     }
   };
   
