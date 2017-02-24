3e6aaea19ee657a8e56397e559969d47ad65e27b
Beau Carnes <beaucarnes@gmail.com>
2017-02-24 00:43:15 +0000

    fixing login upon button click

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -1,21 +1,22 @@
-function updateCount(placeID) {
-  console.log("updateCount called")
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    console.log("request sent")
-    console.log(this.responseText)
-    if (this.readyState == 4 && this.status == 200) {
-      if (document.getElementById(placeID).innerHTML > this.responseText) {
-        document.getElementById(placeID).classList.remove("btn-success");
-        document.getElementById(placeID).classList.add("btn-info");
-      } else {
-        document.getElementById(placeID).classList.remove("btn-info");
-        document.getElementById(placeID).classList.add("btn-success");
-      };
-      document.getElementById(placeID).innerHTML = this.responseText;
-    }
-  };
-  
-  xhttp.open('GET', '/updatecount?id=' + placeID, true);
-  xhttp.send(null);
+function updateCount(placeID, logged) {
+  if (logged) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if (document.getElementById(placeID).innerHTML > this.responseText) {
+          document.getElementById(placeID).classList.remove("btn-success");
+          document.getElementById(placeID).classList.add("btn-info");
+        } else {
+          document.getElementById(placeID).classList.remove("btn-info");
+          document.getElementById(placeID).classList.add("btn-success");
+        };
+        document.getElementById(placeID).innerHTML = this.responseText;
+      }
+    };
+    
+    xhttp.open('GET', '/updatecount?id=' + placeID, true);
+    xhttp.send(null);
+  } else {
+    window.location.href = '/auth/twitter';
+  }
 }
